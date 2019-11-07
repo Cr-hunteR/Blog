a0bce590e08813959f6c12a0832c7d5b81308116
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2019-11-07 19:13:19 +0530

    navStyles

--- js/admin.js
+++ js/admin.js
@@ -1,7 +1,7 @@
 /* new navigation (circular dropdown) */
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
+const links = document.querySelectorAll(".nav-links a");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
