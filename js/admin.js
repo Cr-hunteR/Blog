e860e90f0146aa12a98995e3d9623dd5ceff22ee
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2019-11-07 16:49:20 +0530

    Navigation Completed

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,16 @@
+/* new navigation (circular dropdown) */
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  links.forEach(link=>{
+      link.classList.toggle("fade");
+  });
+ 
+});
+/* End  of new navigation */
 
 // Toggle side menu Function
 
